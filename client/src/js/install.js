a90adf1388f7ece838ff07e2fa0309bbce861dac
sophia altaf <Sophia@sophias-mbp.home>
2023-03-30 15:16:33 +0100

    changes to webpack.js

---
+++ client/src/js/install.js
@@ -0,0 +1,35 @@
+const butInstall = document.getElementById('buttonInstall');
+
+// Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.deferredPrompt = event;
+    butInstall.classList.toggle("hidden", false);
+
+
+});
+
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    const promtEvent = window.deferredprompt;
+
+    if (!promptEvent) {
+        return;
+    }
+    promptEvent.prompt();
+    const result = await promptEvent.userChoice;
+    if (result.outcome === "accepted") {
+        console.log("User accepted the installation");
+    } else {
+        console.log("User dismissed the installation");
+    }
+    window.deferredPrompt = null;
+    butInstall.classList.toggle("hidden", true);
+});
+
+// TODO: Add a handler for the `appinstalled` event
+window.addEventListener("appinstalled", (event) => {
+    console.log("App was successfully installed");   
+    window.deferredprompt =null;
+    butInstall.classList.toggle("hidden", true);
+});
